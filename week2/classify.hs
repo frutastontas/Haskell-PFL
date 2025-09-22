305e88c2839fe4ecf19ad64d9dc14ce51bb8faf2
frutastontas <joaopedrofurtado2005@gmail.com>
2025-09-22 09:55:58 +0100

    week 2

--- week2/classify.hs
+++ week2/classify.hs
@@ -6,3 +6,11 @@ classify xs = if xs <= 20 && xs > 18 then "excellent"
               else "failed"
 -- this definition is using conditionals and not guards
 
+classifyalt :: Int -> String
+classifyalt xs
+    | xs <= 20 && xs > 18 = "excellent"
+    | xs <= 18 && xs > 15 = "very good"
+    | xs <= 15 && xs > 12 = "good"
+    | xs <= 12 && xs > 9 = "passed"
+    | otherwise           = "failed"
+

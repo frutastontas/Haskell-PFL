f7c5d4d7cd663eed68fdae8179265357ddd75b46
frutastontas <joaopedrofurtado2005@gmail.com>
2025-09-22 09:44:05 +0100

    week 2

---
+++ week2/classify.hs
@@ -0,0 +1,8 @@
+classify :: Int-> String
+classify xs = if xs <= 20 && xs > 18 then "excellent"
+              else if xs <= 18 && xs >15 then "very good"
+              else if xs <= 15 && xs > 12 then "good"
+              else if xs <= 12 && xs > 9 then "passed"
+              else "failed"
+-- this definition is using conditionals and not guards
+

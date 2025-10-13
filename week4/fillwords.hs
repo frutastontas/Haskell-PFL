d647c05dfd87ab2c24565131e59542f6d0d20908
frutastontas <joaopedrofurtado2005@gmail.com>
2025-10-13 09:38:05 +0100

    FillWords

--- week4/fillwords.hs
+++ week4/fillwords.hs
@@ -3,32 +3,27 @@ type AWord = String
 type Line= [AWord]
 type Paragraph = [Line]
 
-fitLine :: Int -> [AWord] -> ([AWord], [AWord])
 
-fitLine width xs = loop xs 0 []
-  where
-    loop [] _ acc = (reverse acc, [])
-    loop (w:ws) currWidth acc
-      | currWidth + length w <= width
-          = loop ws (currWidth + length w) (w : acc)
-      | otherwise
-          = (reverse acc, w:ws)
 
 
 fillWords :: Int -> [AWord] -> Paragraph
 fillWords _ [] = []
-fillWords w ws =
-  let (line, remain) = fitLine w ws
-  in if null line
-     then []                     
-     else line : fillWords w remain
+fillWords maxWidth words = loop words [] 0 []
+  where
+    loop [] currentLine _ acc = reverse (reverse currentLine : acc) -- base case: if we have no words left to take we put our current line in the acc and then return it
+    loop (w:ws) currentLine currentLength acc
+      | currentLength == 0 = loop ws (w:currentLine) (length w) acc
+      | (currentLength + 1 + length w) <= maxWidth = loop ws (w:currentLine) (currentLength + 1 + length w) acc -- if word fits then put it in the current line
+      | otherwise = loop (w:ws) [] 0 (reverse currentLine : acc)  -- if word does not fit then we reset out currentLine and Length and add out line we just built into our acc
+
 
 
 
 main :: IO ()
 main = do  
-    input <- getContents
+    input <- getLine
     let output = unlines (map unwords (fillWords 70 (words input)))
+    putStr "This is how your line appears as a paragraph"
     putStr output
     
 
